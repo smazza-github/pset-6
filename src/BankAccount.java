a3bf79ec8bb1246fc9b241eb6b674b21ce19e693
unknown <smazza@ucvts.org>
2019-11-25 19:41:37 -0500

    Finished BankAccount.java tutorial

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,33 +1,79 @@
 public class BankAccount {
-        
+
     private int pin;
     private long accountNo;
     private double balance;
     private User accountHolder;
-    
+
+    public BankAccount(int pin, long accountNo, double balance, User accountHolder) {
+
+      this.pin = pin;
+      this.accountNo = accountNo;
+      this.balance = balance;
+      this.accountHolder = accountHolder;
+
+    }
+
+    public int getPin() {
+
+      return pin;
+
+    }
+
+    public long getAccountNo() {
+
+      return accountNo;
+
+    }
+
+    public double getBalance() {
+
+      return balance;
+
+    }
+
+    public User getAccocuntHolder() {
+
+      return accountHolder;
+
+    }
+
+    public void deposit(double amount) {
+
+      balance = balance + amount;
+
+    }
+
+    public void withdraw(double amount) {
+
+      balance = balance - amount;
+
+    }
+  }
+  
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
     //                                                                        //
     ////////////////////////////////////////////////////////////////////////////
-    
+
     /*
      * Formats the account balance in preparation to be written to the data file.
-     * 
+     *
      * @return a fixed-width string in line with the data file specifications.
      */
-    
+
     private String formatBalance() {
     	return String.format("%.2f", balance);
     }
-    
+
     /*
      * Converts this BankAccount object to a string of text in preparation to
      * be written to the data file.
-     * 
+     *
      * @return a string of text formatted for the data file
      */
-    
+
     @Override
     public String toString() {
         return String.valueOf(accountNo) +
